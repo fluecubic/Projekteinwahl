9e0360d17b4ced6cf7ac2aa4a19f0a5f9f33cbc2
fluecubic <jonas.lorenz1234567890@gmail.com>
2025-10-08 21:01:21 +0200

    tr

---
+++ script.js
@@ -0,0 +1,73 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";  
+import { getDoc, addDoc, doc, getFirestore, getDocs, getDocFromCache, collection, updateDoc, Timestamp, onSnapshot, query, orderBy, serverTimestamp, deleteDoc, arrayUnion   } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+
+
+
+
+
+let Vorname;
+let Nachname;
+let Name;
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0Xp9IRIk9FzDrIEDIlVUUbW4nI31ebb4",
+  authDomain: "projekteinwahl.firebaseapp.com",
+  projectId: "projekteinwahl",
+  storageBucket: "projekteinwahl.firebasestorage.app",
+  messagingSenderId: "198530664417",
+  appId: "1:198530664417:web:14cb7e9137d3cbbe9474a0",
+  measurementId: "G-B47SX5TV37"
+};
+
+const db = getFirestore(initializeApp(firebaseConfig));
+
+if (!localStorage.getItem("Name") || localStorage.getItem("Name") == "") {
+    UI("logedout")
+     document.getElementById("go").addEventListener("click", async function () {
+     Vorname = document.getElementById("name").value;
+     Nachname = document.getElementById("surname").value;
+     Name = Vorname + " " + Nachname;
+
+     localStorage.setItem("Name", Name)
+     localStorage.setItem("Vorname", Vorname)
+     localStorage.setItem("Nachname", Nachname)
+
+     const q = query(collection(db, "Users")); 
+
+     const querySnapshot = await getDocs(q);
+
+    
+     UI("choose")
+})   
+    } else {
+     Name = localStorage.getItem("Name");
+     Vorname = localStorage.getItem("Vorname");
+     Nachname = localStorage.getItem("Nachname");
+     UI("choose")
+    }
+
+    function UI(status) {
+        if (status == "logedout") {
+            document.getElementById("name").style.display = "block"
+            document.getElementById("surname").style.display = "block"
+            document.getElementById("go").style.display = "block"
+            document.getElementsByClassName("info")[0].style.display = "block"
+            document.getElementsByClassName("info")[1].style.display = "block"
+            document.getElementsByClassName("info")[2].style.display = "block"
+            document.getElementById("projects").style.display = "none"
+        }
+
+        if (status == "choose") {
+            document.getElementById("name").style.display = "none"
+            document.getElementById("surname").style.display = "none"
+            document.getElementById("go").style.display = "block"
+            document.getElementsByClassName("info")[0].innerHTML = "Hallo, " + Name + ". Wähle ein Projekt"
+            document.getElementsByClassName("info")[1].style.display = "none"
+            document.getElementsByClassName("info")[2].style.display = "none"
+            document.getElementById("projects").style.display = "block"
+        }
+    }
+
+    
